c116d1491a029def1cbf893dc0cf3dd20247a528
Roi <vosroix@gmail.com>
2020-02-20 12:08:40 +0500

    added a menu togggle btn

--- js/main.js
+++ js/main.js
@@ -40,3 +40,14 @@ $(function() {
         }
     }
 });
+
+/* menu-toggle */
+
+$(function() {
+    $("#menu-toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#menu__list").toggleClass("active");
+    });
+});
