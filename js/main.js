9b9521afa76c20539364dfc11f075e832ed95419
Roi <vosroix@gmail.com>
2020-02-20 11:52:22 +0500

    js code transferred

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+//Mask for phone input
+$(function(){ $("#telephone").mask("+7(999)-999-99-99"); });
+
+$('input[type="tel"]').click(function(){
+    $(this).setCursorPosition(3); 
+});
+
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+        $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+        var range = $(this).get(0).createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+    }
+};
+
+// Fixing header
+
+$(function() {
+    let header = $("#header-top");
+    let intorH = $("#header").innerHeight();
+    let scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= intorH) {
+            header.addClass("header-top--fixed");
+        } else {
+            header.removeClass("header-top--fixed");
+        }
+    }
+});
